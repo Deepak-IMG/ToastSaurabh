1704882cbea63f8f246217f14d0e42fbffd65cbb
Deepak Gormat <114463615+Deepak-IMG@users.noreply.github.com>
2023-12-14 17:13:12 +0530

    first commit

---
+++ saurabh/src/main/java/com/img/saurabh/ToastSaurabh.kt
@@ -0,0 +1,10 @@
+package com.img.saurabh
+
+import android.content.Context
+import android.widget.Toast
+
+class ToastSaurabh{
+     fun saurabh(context: Context, yourMessage: String){
+         Toast.makeText(context, yourMessage, Toast.LENGTH_SHORT).show()
+     }
+}
